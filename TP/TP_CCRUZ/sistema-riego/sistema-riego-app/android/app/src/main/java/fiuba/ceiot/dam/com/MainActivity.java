669654c530f7f7b642e6be8255c4a7ffc059885b
geekmoments <70829024+geekmoments@users.noreply.github.com>
2021-08-29 20:21:59 -0500

    configuración de capacitor para android e ios

---
+++ TP/TP_CCRUZ/sistema-riego/sistema-riego-app/android/app/src/main/java/fiuba/ceiot/dam/com/MainActivity.java
@@ -0,0 +1,5 @@
+package fiuba.ceiot.dam.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- TP/TP_CCRUZ/sistema-riego/sistema-riego-app/capacitor.config.ts
+++ TP/TP_CCRUZ/sistema-riego/sistema-riego-app/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'sistema-riego-app',
+  appId: 'fiuba.ceiot.dam.com',
+  appName: 'TP_SR_APP',
   webDir: 'www',
   bundledWebRuntime: false
 };
